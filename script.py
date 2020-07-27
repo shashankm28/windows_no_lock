d9be006f2bc045cbe7a87053196bbc87fc61595f
Shashank Mishra <shashank-mishra@hotmail.com>
2020-07-27 16:56:55 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,29 @@
+## This script will simulate a keypress and prevent Windows from locking
+
+import pyautogui
+import time
+
+def no_lock(button):
+    try:
+        while True:
+            pyautogui.press(button)
+            time.sleep(2)
+            pyautogui.press(button)
+            time.sleep(3)
+
+    except Exception as ex:
+        print ('no_lock | Error: ', ex)
+
+def main():
+    try:
+        print ('Prevent Windows lock')
+        kb_button = input('Enter keyboard button: ')
+        
+        print ('Running')
+        no_lock(kb_button)
+
+    except Exception as ex:
+        print ('main | Error: ', ex)
+
+if __name__ == "__main__":
+    main()
